794b84f79b16d830ef3d3c7a1dcb0bb484f28879
unknown <eranda41@yahoo.com>
2019-08-01 14:21:36 +1000

    basic setup to fetch from an api webpack proxy to avoid CORB

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { properties: [], isLoaded: false };
+    //this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(
+      "/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f",
+      {
+        method: 'GET',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json',},
+      }
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result)
+
+          this.setState({
+            isLoaded: true,
+            properties: result.properties
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    return (this.state.properties.length > 0 && this.state.properties.map((property, index) => {
+      return <li key={index}>{property.summary}</li>;
+    }));
+  }
 }
+
 
 export default App;
