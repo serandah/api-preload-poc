7793c7c408e899dd2ac33dfd86a537cab806aef3
unknown <eranda41@yahoo.com>
2019-08-05 15:28:02 +1000

    create a generic component to load realestate properties that can be nested

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ApiPreload from 'api-preload'
-import LevelTwo from './LevelTwo'
+import Properties from './Properties'
 import './App.css';
 
 class App extends React.Component {
@@ -13,53 +13,28 @@ class App extends React.Component {
       '/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f&maxDaysSinceAdded=1&minBedrooms=2&maxBedrooms=2'
     ])
     preload.load()
-    // Don't call this.setState() here!
     this.state = { properties: [], isLoaded: false };
-    //this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    fetch(
-      "/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f",
-      {
-        method: 'GET'
-      }
-    )
-      .then(res => res.json())
-      .then(
-        result => {
-          console.log(result)
-
-          this.setState({
-            isLoaded: true,
-            properties: result.properties
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-
-      );
   }
 
   render() {
-    return this.state.properties.length > 0 && <div>
-      <h2>London property listings</h2>
-      <ul>
-        {this.state.properties.map((property, index) => {
-          return <li key={index}>{property.summary}</li>;
-        })}
-      </ul>
-      <LevelTwo />
-    </div>
+    return <Properties
+      title={'London property listings'}
+      fetch={{ url: '/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f', options: {} }}
+      numItems={5}
+    >
+      <Properties
+        title={'From Yesterday'}
+        fetch={{ url: '/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f&maxDaysSinceAdded=1', options: {} }}
+        numItems={3}
+      >
+        <Properties
+          title={'With two bedrooms'}
+          fetch={{ url: '/for-sale/london/?key=82db11669fa84f4ead487f77152dd51f&maxDaysSinceAdded=1&minBedrooms=2&maxBedrooms=2', options: {} }}
+          numItems={3}
+        />
+      </Properties>
+    </Properties>
   }
 }
-
 
 export default App;
